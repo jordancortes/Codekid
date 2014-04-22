341145efa8755aea971e00dc4690131c57c1bb0b
Jordan Cortes Guzman <jordancg91@gmail.com>
2014-04-21 23:05:57 -0500

    DropZoneTextFields solo aceptan números
    También se bloquean las acciones de Select, SelectAll, Copy y Paste
    sobre estos TextFields

--- codekid/DropZoneTextField.m
+++ codekid/DropZoneTextField.m
@@ -25,62 +25,69 @@
 
 - (void) textFieldText:(NSNotification *)notification
 {
-    if ([[self text] length] > _last_length && [[self text] length] > 1)
+    NSScanner *scanner = [NSScanner scannerWithString:[self text]];
+
+    if ([scanner scanInteger:NULL] && [scanner isAtEnd])
     {
-        // Incrementa el tamaño del main_view
-        CGRect main_view_frame = [[self superview] superview].frame;
-        main_view_frame.size.width += INNER_TEXT_INCREMENT;
-        [[self superview] superview].frame = main_view_frame;
-        
-        // Mueve el resto de los views del lado derecho
-        for (UIView *this_view in [[self superview] superview].subviews)
+        if ([[self text] length] > _last_length && [[self text] length] > 1)
         {
-            if ([this_view tag] > [[self superview] tag])
+            // Incrementa el tamaño del main_view
+            CGRect main_view_frame = [[self superview] superview].frame;
+            main_view_frame.size.width += INNER_TEXT_INCREMENT;
+            [[self superview] superview].frame = main_view_frame;
+            
+            // Mueve el resto de los views del lado derecho
+            for (UIView *this_view in [[self superview] superview].subviews)
             {
-                CGRect this_view_frame = this_view.frame;
-                this_view_frame.origin.x += INNER_TEXT_INCREMENT;
-                this_view.frame = this_view_frame;
+                if ([this_view tag] > [[self superview] tag])
+                {
+                    CGRect this_view_frame = this_view.frame;
+                    this_view_frame.origin.x += INNER_TEXT_INCREMENT;
+                    this_view.frame = this_view_frame;
+                }
             }
+            
+            // Incrmenta el tamaño del inner_view donde esta el UITextField
+            CGRect inner_view_frame = [self superview].frame;
+            inner_view_frame.size.width += INNER_TEXT_INCREMENT;
+            [self superview].frame = inner_view_frame;
+            
+            // Incrementa el tamaño del UITextField
+            [self resizeToFitView:[self superview]];
+        }
+        else if ([[self text] length] < _last_length && [[self text] length] > 0)
+        {
+            // Disminuye el tamaño del main_view
+            CGRect main_view_frame = [[self superview] superview].frame;
+            main_view_frame.size.width -= INNER_TEXT_INCREMENT;
+            [[self superview] superview].frame = main_view_frame;
+            
+            // Mueve el resto de los views del lado derecho
+            for (UIView *this_view in [[self superview] superview].subviews)
+            {
+                if ([this_view tag] > [[self superview] tag])
+                {
+                    CGRect this_view_frame = this_view.frame;
+                    this_view_frame.origin.x -= INNER_TEXT_INCREMENT;
+                    this_view.frame = this_view_frame;
+                }
+            }
+            
+            // Incrmenta el tamaño del inner_view donde esta el UITextField
+            CGRect inner_view_frame = [self superview].frame;
+            inner_view_frame.size.width -= INNER_TEXT_INCREMENT;
+            [self superview].frame = inner_view_frame;
+            
+            // Incrementa el tamaño del UITextField
+            [self resizeToFitView:[self superview]];
         }
         
-        // Incrmenta el tamaño del inner_view donde esta el UITextField
-        CGRect inner_view_frame = [self superview].frame;
-        inner_view_frame.size.width += INNER_TEXT_INCREMENT;
-        [self superview].frame = inner_view_frame;
-        
-        // Incrementa el tamaño del UITextField
-        [self resizeToFitView:[self superview]];
+        [self setLast_length:[[self text] length]];
     }
-    else if ([[self text] length] < _last_length && [[self text] length] > 0)
+    else if ([[self text] length] > 0)// no es número, por lo tanto lo borra
     {
-        NSLog(@"Decrece\n");
-        
-        // Disminuye el tamaño del main_view
-        CGRect main_view_frame = [[self superview] superview].frame;
-        main_view_frame.size.width -= INNER_TEXT_INCREMENT;
-        [[self superview] superview].frame = main_view_frame;
-        
-        // Mueve el resto de los views del lado derecho
-        for (UIView *this_view in [[self superview] superview].subviews)
-        {
-            if ([this_view tag] > [[self superview] tag])
-            {
-                CGRect this_view_frame = this_view.frame;
-                this_view_frame.origin.x -= INNER_TEXT_INCREMENT;
-                this_view.frame = this_view_frame;
-            }
-        }
-        
-        // Incrmenta el tamaño del inner_view donde esta el UITextField
-        CGRect inner_view_frame = [self superview].frame;
-        inner_view_frame.size.width -= INNER_TEXT_INCREMENT;
-        [self superview].frame = inner_view_frame;
-        
-        // Incrementa el tamaño del UITextField
-        [self resizeToFitView:[self superview]];
+        [self setText:[[self text] substringToIndex:([[self text] length] - 1)]];
     }
-    
-    [self setLast_length:[[self text] length]];
 }
 
 - (void)resizeToFitView:(UIView *)this_view
@@ -90,11 +97,7 @@
 
 - (BOOL)canPerformAction:(SEL)action withSender:(id)sender
 {
-    if (action == @selector(paste:))
-    {
-        return NO;
-    }
-    else if (action == @selector(copy:))
+    if (action == @selector(paste:) || action == @selector(copy:) || action == @selector(select:) || action == @selector(selectAll:))
     {
         return NO;
     }
