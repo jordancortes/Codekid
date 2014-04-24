43843995a9670162f1aca620f73a6046cc8b18d6
Jordan Cortes Guzman <jordancg91@gmail.com>
2014-04-24 12:53:43 -0500

    Se elimina método viejo que modificaba dimensiones

--- codekid/Block.h
+++ codekid/Block.h
@@ -8,16 +8,10 @@
 
 #import <Foundation/Foundation.h>
 
-// TODO: ya no son fijos, ver BlockFactory.h
-#define INNER_DROP_ZONE_WIDTH 40
-#define INNER_DROP_ZONE_HEIGHT 59
-
 @interface Block : NSObject
 
 @property UIView *main_view;
 @property NSMutableArray *inner_drop_zones;
 
-//- (void)setBlockInsideTo:(BOOL)value ForView:(UIView *)this_view;
-- (void)increaseSize:(CGFloat)size FromTag:(NSInteger)this_tag;
 - (BOOL)isChildOfView:(UIView *)this_view;
 @end
--- codekid/Block.m
+++ codekid/Block.m
@@ -10,30 +10,6 @@
 
 @implementation Block
 
-- (void)increaseSize:(CGFloat)size FromTag:(NSInteger)this_tag
-{
-    // incrementa el tamaño de main_view
-    CGRect main_view_frame = _main_view.frame;
-    main_view_frame.size.width = main_view_frame.size.width - INNER_DROP_ZONE_WIDTH + size;
-    _main_view.frame = main_view_frame;
-
-    for (UIView *this_view in _main_view.subviews)
-    {
-        if ([this_view tag] == this_tag) // incrementa el tamaño del inner_view al que se agrega
-        {
-            CGRect this_view_frame = this_view.frame;
-            this_view_frame.size.width = size;
-            this_view.frame = this_view_frame;
-        }
-        else if ([this_view tag] > this_tag) // recorre el resto de los inner_view
-        {
-            CGRect this_frame = this_view.frame;
-            this_frame.origin.x = this_frame.origin.x - INNER_DROP_ZONE_WIDTH + size;
-            this_view.frame = this_frame;
-        }
-    }
-}
-
 - (BOOL)isChildOfView:(UIView *)this_view
 {
     UIView *check_view = [self main_view];
